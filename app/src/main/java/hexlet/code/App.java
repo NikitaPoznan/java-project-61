669bcbe19559b6b00b5faa168af83f184cfa17fd
NikitaPoznan <pz.nikita2015@yandex.ru>
2023-03-14 18:23:57 +0300

    init

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,24 @@
 package hexlet.code;
-import hexlet.code.Cli;
+
+
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        String games = scanner.next();
+
+        switch (games) {
+            case "1" : Cli.greeting();
+            case "2" : Even.start();
+
+        }
     }
 }
