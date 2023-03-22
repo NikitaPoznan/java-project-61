b09c6d71595c583dc73515d9806ee8b82985a157
NikitaPoznan <pz.nikita2015@yandex.ru>
2023-03-22 23:13:56 +0300

    correction Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-
+    public static final int ROUND_COUNT= 3;
     public static void runGames(String rule, String[][] gameRounds) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
@@ -12,17 +12,17 @@ public class Engine {
         System.out.println("Hello, " + userName + "!");
         System.out.println(rule);
 
-        for (var process : gameRounds) {    // два цикла для генеарции вопросов и для отыгрыша
-            System.out.println("Question:" + process[0]);
+        for (var result : gameRounds) {    // два цикла для генеарции вопросов и для отыгрыша
+            System.out.println("Question:" + result[0]);
             System.out.println("Answer:");
             String answer = scanner.next();
 
-            if (answer.equalsIgnoreCase(process[1])) {
+            if (answer.equalsIgnoreCase(result[1])) {
                 System.out.println("Correct!");
             } else {
-                System.out.println(answer + " is wrong answer ;(. Correct answer was .\n" + process[1] +
+                System.out.println(answer + " is wrong answer ;(. Correct answer was .\n" + result[1] +
                         " Let's try again," + userName);
-                return;
+                 return;
 
             }
         }
