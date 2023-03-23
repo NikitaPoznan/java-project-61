96ed230001afe9cde1e3f5187fee948446c642ef
NikitaPoznan <pz.nikita2015@yandex.ru>
2023-03-23 15:38:11 +0300

    create Prime

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,7 @@
 package hexlet.code;
 
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import  hexlet.code.games.GCD;
+import hexlet.code.games.*;
 import hexlet.code.games.Progression;
 
 
@@ -19,6 +17,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GDC");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         String games = scanner.next();
@@ -29,6 +28,7 @@ public class App {
             case "3" -> Calc.start();
             case "4" -> GCD.start();
             case "5" -> Progression.start();
+            case "6" -> Prime.start();
             default -> System.out.println("invalid Input");
         }
     }
---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,45 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+import static hexlet.code.Engine.ROUND_COUNT;
+
+public class Prime {
+    public static void start() {
+        var rule = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String[][] gameRounds = new String[ROUND_COUNT][];  // вопрос, ответ , вопрос , ответ
+        // параметры методов и возвращаемое значение методов(повторить!) передача параметров по ссылке и по значению!
+
+        for (var i = 0; i < ROUND_COUNT; i++) {
+            gameRounds[i] = generateRound();
+
+            // создать константу, так как упоминается в нескольких местах. Это позволяет изменить только одну константу
+        }
+        Engine.runGames(rule, gameRounds);
+    }
+
+    private static String[] generateRound() {
+        String[] result= new String[2];   // генерация раундов, вопрос и ответ (генерация под массива)
+        Random rand = new Random();
+        int number = rand.nextInt(102);
+        result[0] = String.valueOf(number);
+        result[1] = countPrime(number) ? "yes" : "no";
+        return result;
+    }
+
+    private static boolean countPrime(int number) {
+        if(number <=1){
+            return false;
+        }
+
+        for (int i = 2; i <= number/2 ; i++) {
+      if(number % i == 0){
+          return false;
+      }
+
+        }
+        return true;
+    }
+}
