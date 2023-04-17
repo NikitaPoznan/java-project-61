ea0501e39de9e9c9fc1e5f0b64cd37013be4b241
NikitaPoznan <pz.nikita2015@yandex.ru>
2023-04-17 18:27:36 +0300

    fix whitespace Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class Utils {
     private static final Random RAND = new Random();
-    public static int generateNumber(int a){
+
+    public static int generateNumber(int a) {
 
         return RAND.nextInt(a);
     }
